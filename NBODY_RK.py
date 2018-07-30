a7eb409ef531e621c8baadccefa0afbd82388ad5
metraf <8030379+Metraf@users.noreply.github.com>
2018-07-30 12:12:04 -0400

    Working with nbodies
    But might not be affected by planet's attraction

--- NBODY_RK.py
+++ NBODY_RK.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
 import math
+import random
 
-nb_annees = 1000
+nb_objets = 50
+nb_annees = 200
 nb_UA = 5
-intervalle_nb_jours = 1
+intervalle_nb_jours = 2
+loin = 1.2  # éloignement des pltl / planete
 # excentricity
 e = 0.02
 e_pltl = 0
-a_fois_pltl = 0.9
 # constants :
 constG = 6.67408 * 10 ** (-11)
 nbiter = 31536000 * nb_annees
 a = 149597900000.0 * nb_UA
-a_pltl = a * a_fois_pltl
 intervalle = 86400 * intervalle_nb_jours
 
+
 # Working on NBODY_RK
+
+
+def rand_pos():  # used to create randomly initially placed planetesimals around the star
+
+    rand_x = random.choice((random.randrange(-loin * a, -a, 500), random.randrange(a, loin * a, 500)))
+    rand_y = random.choice((random.randrange(-loin * a, -a, 500), random.randrange(a, loin * a, 500)))
+    return rand_x, rand_y
 
 
 class Planetesimal:
@@ -26,26 +35,28 @@ class Planetesimal:
     mu = constG * star_mass
 
     def __init__(self):
-
+        self.posx, self.posy = rand_pos()  # defining initial position
+        self.a_pltl = math.sqrt(self.posx ** 2 + self.posy ** 2)
         self.tolerance = 1e-14
         self.dt = intervalle
 
         # orbital param initialisation
         self.dE = self.tolerance + 1
-        self.M = math.fmod(self.dt * math.sqrt(self.mu / a_pltl ** 3), 2 * math.pi)
+        self.M = math.fmod(math.atan(self.posy / self.posx) +
+                           self.dt * math.sqrt(self.mu / math.sqrt(self.posx ** 2 + self.posy ** 2) ** 3), 2 * math.pi)
+
         self.E = self.E0 = self.M
         while self.dE > self.tolerance:
             self.E = self.E - (self.E - e_pltl * math.sin(self.E) - self.M) / (1 - e_pltl * math.cos(self.E))
             self.dE = math.fabs(self.E - self.E0)
             self.E0 = self.E
 
-        self.r = a_pltl * (1 - e_pltl * math.cos(self.E))
-        self.nu = 2 * math.atan2(math.sqrt(1 + e_pltl) * math.sin(self.E / 2), math.sqrt(1 - e_pltl) * math.cos(self.E / 2))
+        self.r = self.a_pltl * (1 - e_pltl * math.cos(self.E))
+        self.nu = 2 * math.atan2(math.sqrt(1 + e_pltl) * math.sin(self.E / 2),
+                                 math.sqrt(1 - e_pltl) * math.cos(self.E / 2))
 
-        self.posx = a_pltl
-        self.posy = 0
-        self.vx = -math.sin(self.E) * math.sqrt(self.mu * a_pltl) / self.r
-        self.vy = math.sqrt(1 - e_pltl ** 2) * math.cos(self.E) * math.sqrt(self.mu * a_pltl) / self.r
+        self.vx = -math.sin(self.E) * math.sqrt(self.mu * self.a_pltl) / self.r
+        self.vy = math.sqrt(1 - e_pltl ** 2) * math.cos(self.E) * math.sqrt(self.mu * self.a_pltl) / self.r
         # end of orbital initialisation param
 
     def __iter__(self):
@@ -53,12 +64,12 @@ class Planetesimal:
 
     def accel_planetesimal(self, posx, posy):
         # acceleration of one planetesimal
-        return [(-posx * constG * self.star_mass / (math.sqrt(posx ** 2 + posy ** 2) ** 3)) + ((
-                    planete.posx - posx) * constG * self.plan_mass / (
-                            math.sqrt((planete.posx - posx) ** 2 + (planete.posy - posy) ** 2) ** 3)),
-                (-posy * constG * self.star_mass / (math.sqrt(posx ** 2 + posy ** 2) ** 3)) + ((
-                            planete.posy - posy) * constG * self.plan_mass / (
-                            math.sqrt((planete.posx - posx) ** 2 + (planete.posy - posy) ** 2) ** 3))]
+        return ((-posx * constG * self.star_mass / (math.sqrt(posx ** 2 + posy ** 2) ** 3)) +
+                ((planete.posx - posx) * constG * self.plan_mass / (math.sqrt((planete.posx - posx) ** 2 +
+                                                                              (planete.posy - posy) ** 2) ** 3)),
+                (-posy * constG * self.star_mass / (math.sqrt(posx ** 2 + posy ** 2) ** 3)) +
+                ((planete.posy - posy) * constG * self.plan_mass / (math.sqrt((planete.posx - posx) ** 2 +
+                                                                              (planete.posy - posy) ** 2) ** 3)))
 
     def __next__(self):
         # k1
@@ -93,8 +104,6 @@ class Planetesimal:
         self.posy = self.posy + self.dt * (self.k1py + 2 * self.k2py + 2 * self.k3py + self.k4py) / 6
 
         return self
-
-
 
 
 class Planete:
@@ -137,7 +146,6 @@ class Planete:
                 -posy * constG * self.star_mass / (math.sqrt(posx ** 2 + posy ** 2) ** 3)]
 
     def __next__(self):
-
         # k1
         self.k1s = self.accel(self.posx, self.posy)
 
@@ -193,31 +201,35 @@ coordx, coordy = [], []  # lists containing coordinates of the planet
 # planete.error1()
 mvt = iter(planete)  # iter launch
 
-#planetesimal setting
-coordx_pltl, coordy_pltl = [], [] #lists containing planetesimal's coordinates
-planetesimal1 = Planetesimal()
-mvt_pltl = iter(planetesimal1)
+# planetesimal setting
+coordx_pltl, coordy_pltl = [], []  # lists containing planetesimal's coordinates
 
-while True:
+planetesimal = []
+mvt_pltl = []
+
+for i in range(nb_objets):
+    planetesimal.append(Planetesimal())
+    mvt_pltl.append(iter(planetesimal[i-1]))
+
+while planete.t < nbiter:
 
     coordx.append(planete.posx)
     coordy.append(planete.posy)
-    coordx_pltl.append(planetesimal1.posx)
-    coordy_pltl.append(planetesimal1.posy)
 
+    for i in range(nb_objets):
+        coordx_pltl.append(planetesimal[i-1].posx)
+        coordy_pltl.append(planetesimal[i-1].posy)
+        next(planetesimal[i - 1])
 
     next(mvt)
-    next(planetesimal1)
-    if math.sqrt(planete.posx ** 2 + planete.posy ** 2) < 1400000:
-        break
 
-    if planete.t > nbiter:
+    if math.sqrt(planete.posx ** 2 + planete.posy ** 2) < 1400000:  # collision
         break
 
 # planete.error2()
 # print("dE =", planete.de_tot)
 plt.grid(True)
 plt.plot(coordx, coordy, linestyle='-.')  # planet's coordinates
-plt.plot(coordx_pltl, coordy_pltl, linestyle='-.', color='g')  # planetesimals coordinates
+plt.plot(coordx_pltl, coordy_pltl, linestyle='None', color='g', marker=',')  # planetesimals coordinates
 plt.plot([0], marker='o', color='r')  # star's coordinates
 plt.show()
